30d27fc01a89b259d14db93ca5ac6f207f852bd3
Deepakpal0807 <Deepakpal981790@gmail.com>
2024-10-07 19:44:11 +0530

    mongodbatlasconnectiondone

--- server/app.js
+++ server/app.js
@@ -7,13 +7,14 @@ import router1 from './routes/download.route.js'
 const app = express();
 
 // Enable CORS
-app.use(cors(
-  {
-    origin:["https://deploy-mern--1whq.vercel.app"],
-    methods:["POST","GET"],
-    credential:true
-  }
-));
+// app.use(cors(
+//   {
+//     origin:["https://deploy-mern--1whq.vercel.app"],
+//     methods:["POST","GET"],
+//     credential:true
+//   }
+// ));
+app.use(cors())
 
 // Parse incoming JSON requests (if necessary)
 app.use(express.json()); // For JSON payloads
